5cfcae91751dd12a3522e01ce36e557b97a48041
Gennadya <heavyyrain3039@gmail.com>
2019-05-10 19:31:51 +0300

    heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss', './style.components.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
